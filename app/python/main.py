a366782bfdb93b295d4cd9433cb3dc60a8aa99fe
ynishi18 <34960996+ynishi18@users.noreply.github.com>
2023-04-13 06:15:17 +0000

    update. フロントエンドからの通信を許可するためにバックエンドにCORSを設定

--- app/python/main.py
+++ app/python/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from machine_learning.titanic import PredictOnAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['http://localhost:3000'],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 @app.get('/helloworld')
 def get_hello_message():
