aafac0f0f10ad9a1339ee330cdc04d547f11767f
ynishi18 <34960996+ynishi18@users.noreply.github.com>
2023-04-13 06:12:29 +0000

    update. axiosによるバックエンドへの非同期通信処理を追加

--- app/node/tutorial/src/App.vue
+++ app/node/tutorial/src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
- import {reactive} from 'vue'
+ import {ref, reactive} from 'vue'
+ import axios from 'axios'
+
  // 型を定義する場合は冒頭に type を使う (interface を使った書き方もある。その場合は "=" を外す)
  type typeOfFeatures = {
   Sex: '男性'|'女性'|'性別は？'
@@ -17,15 +19,60 @@
    SibSp: '兄弟姉妹の同伴者数は？'
  })
 
+ // 生存確率を格納するための箱を作る
+ const survivalProbability = ref<number|undefined>()
+
+ // バリデーションのための関数を定義。
+ const validateRequestValues = (): boolean => {
+   if (features.Sex == '性別は？') {
+     alert('性別を入力してください。')
+     return false
+   }
+   if (features.Pclass == '階級は？') {
+     alert('階級を入力してください。')
+     return false
+   } 
+   if (features.Age == '年齢は？') {
+     alert('年齢を入力してください。')
+     return false
+   } 
+   if (features.Parch == '親・子の同伴者数は？') {
+     alert('親子同伴者数を入力してください。')
+     return false
+   } 
+   if (features.SibSp == '兄弟姉妹の同伴者数は？') {
+     alert('兄弟姉妹同伴者数を入力してください。')
+     return false
+   }
+   // 全ての条件に引っかからなかった場合にのみ true を返す
+   return true  
+ }
+
  const displayOutput = (): void => {
-   alert(`
-     性別: ${features.Sex}
-     階級: ${features.Pclass} 
-     年齢: ${features.Age} 
-     親・子同伴者数: ${features.Parch} 
-     兄弟姉妹同伴者数: ${features.SibSp}
-   `)
-  }
+   // エンドポイントを指定
+   const endPoint: string = 'http://localhost:5000/api/titanic'
+   // バリデーションの実行
+   const validationResult: boolean = validateRequestValues()
+   // バリデーションに成功していれば axios を実行
+   if (validationResult === true) {  
+     // axios.post で POST メソッドを実行することを指示。
+     // 第一引数にエンドポイント, 第二引数にリクエストボディを指定。
+     axios.post(
+       endPoint, features
+     ).then(
+       // then 以下で問題なくレスポンスが返ってきた際の挙動を記述
+       (response) => {
+         // TypeScriptの型指定は後ろに as をこのように付けることでも可能。
+         survivalProbability.value = 100 * response.data.survival_probability as number
+       }
+     ).catch(
+       // catch 以下では問題があった際の挙動を記述
+       () => {
+         alert('エラーが発生しました。')
+       } 
+     )
+   }
+ }
 </script>
 
 <template>
@@ -66,5 +113,10 @@
    </select> 人
    <br>
    <button class="btn btn-primary" v-on:click="displayOutput()">結果を出力</button>
+   <template v-if="survivalProbability !== undefined">
+     <div class="alert alert-error mt-4 ">
+       あなたの生存確率は {{ Math.round(survivalProbability) }} % です。
+     </div>
+   </template>
  </div>
  </template>
